1c02d6f2865c8434194d7ca93301c43a02815c39
nayburz <>
2020-12-23 00:40:51 -0800

    Added basic search for full name

--- directory/urls.py
+++ directory/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, DirectoryListView, DirectoryDetailView
+from .views import HomePageView, DirectoryListView, DirectoryDetailView, SearchResultsView
 
 app_name = 'directory'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('ptin-directory/', DirectoryListView.as_view(), name='directory-list'),
     path('ptin-directory/<int:pk>', DirectoryDetailView.as_view(), name='person-detail'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
--- directory/views.py
+++ directory/views.py
@@ -1,5 +1,8 @@
 
 from django.views.generic import TemplateView, ListView, DetailView
+from django.db.models import Q, query
+from django.db.models.functions import Concat
+from django.db.models import CharField, Value
 
 from .models import Person
 
@@ -14,3 +17,26 @@ class DirectoryListView(ListView):
 class DirectoryDetailView(DetailView):
     model = Person
     template_name = "person_detail.html"
+
+class SearchResultsView(ListView):
+    model = Person
+    template_name = 'search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        query_list = Person.objects.all()
+        
+
+        if query:
+            query_list = query_list.annotate(
+                full_name=Concat(
+                    'first_name',
+                    Value(' '),
+                    'last_name',
+                    output_field=CharField()
+                )
+            ).filter(
+                Q(full_name__icontains=query)
+
+        )
+        return query_list
